ef9259632ff780dbad4fcce57e8b3b2374c3b6c1
mkalidev <greatpannft@gmail.com>
2025-11-17 22:42:31 +0000

    feat: changes in src/components/layout/Container.jsx

--- src/components/layout/Container.jsx
+++ src/components/layout/Container.jsx
@@ -1,7 +1,8 @@
 import React from 'react'
+import { Outlet } from 'react-router-dom'
 
 export default function Container() {
   return (
-    <div>Container</div>
+    <div className='w-full mx'><Outlet /></div>
   )
 }
