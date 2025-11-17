32ee398b0228b40b4174aef87fb9fc4dcb507b21
mkalidev <greatpannft@gmail.com>
2025-11-17 22:49:31 +0000

    feat: changes in src/pages/Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,16 +1,12 @@
 import React from "react";
-import Container from "../components/layout/Container";
 
 export default function Home() {
   return (
     <>
-      <div className="w-full h-[30px] bg-purple-500">
-        {/* <Container>
-          <div className="w-full h-full bg-red-500">
-            <h1>Home</h1>
-          </div>
-        </Container> */}
-      </div>
+      <p className="text-2xl font-bold">Welcome to the Home Page</p>
+      <p className="text-sm text-gray-500">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quos.
+      </p>
     </>
   );
 }
