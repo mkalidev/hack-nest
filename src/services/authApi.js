d9c5199f7e3dc12b5ce87e4e5a31af6f9de5a29b
mkalidev <greatpannft@gmail.com>
2025-11-17 22:54:16 +0000

    feat: changes in src/services/authApi.js

---
+++ src/services/authApi.js
@@ -0,0 +1,60 @@
+import axios from "axios";
+import { getCookie } from "../lib/utils";
+
+const apiURL = import.meta.env.VITE_API_URL;
+
+export async function signup(body) {
+  try {
+    const { data } = await axios.post(`${apiURL}auth/signup`, body);
+    return data;
+  } catch (error) {
+    console.error("Error during signup:", error);
+    throw new Error(
+      error.response?.data?.message || "An error occurred during signup"
+    );
+  }
+}
+
+export async function verifyEmail(body) {
+  try {
+    const { data } = await axios.post(`${apiURL}auth/verify-email`, body);
+    return data;
+  } catch (error) {
+    console.error("Error during verification:", error);
+    throw new Error(
+      error.response?.data?.message || "An error occurred during verification"
+    );
+  }
+}
+
+export async function login(body) {
+  try {
+    const { data } = await axios.post(`${apiURL}auth/login`, body);
+    return data;
+  } catch (error) {
+    console.error("Error during login:", error);
+    throw new Error(
+      error.response?.data?.message || "An error occurred during login"
+    );
+  }
+}
+
+export async function getCurrentUser() {
+  try {
+    // Get token from cookies
+    const token = getCookie("token"); // or whatever your cookie name is
+
+    const { data } = await axios.get(`${apiURL}auth/me`, {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    });
+    return data;
+  } catch (error) {
+    console.error("Error during while fetching user data", error);
+    throw new Error(
+      error.response?.data?.message ||
+        "An error occurred while fetching user data"
+    );
+  }
+}
