98cbb79865e0d2abcd3c4f8bfb50e0cba6513e21
mkalidev <greatpannft@gmail.com>
2025-11-17 21:49:28 +0000

    feat: changes in src/components/layout/Layout.jsx

--- src/components/layout/Layout.jsx
+++ src/components/layout/Layout.jsx
@@ -1,14 +1,20 @@
-import React from 'react'
-import Navbar from './Navbar'
-import Sidebar from './Sidebar'
-import Footer from './Footer'
+import React from "react";
+import Navbar from "./Navbar";
+import Sidebar from "./Sidebar";
+import Footer from "./Footer";
+import MobileSidebar from "./MobileSidebar";
+import { Outlet } from "react-router-dom";
 
 export default function Layout() {
   return (
-    <div>
-        <Navbar />
-        <Sidebar />
-        <Footer />
+    <div className="flex flex-col min-h-screen">
+      <Navbar />
+      <main className="flex-1">
+        <Outlet />
+      </main>
+      <Sidebar />
+      <Footer />
+      <MobileSidebar />
     </div>
-  )
+  );
 }
