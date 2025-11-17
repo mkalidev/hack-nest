385334e00d3c0a20543d83c9b4b4df1b93d04e7a
mkalidev <greatpannft@gmail.com>
2025-11-17 23:38:34 +0000

    feat: changes in src/main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import App from "./App.jsx";
 import "@fontsource-variable/inter";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { AppKitProvider } from "@reown/appkit/react";
-import { baseSepolia, base, celo, mainnet } from "viem/chains";
+import { baseSepolia, base, celo, mainnet } from "viem/
 const queryClient = new QueryClient();
 
 createRoot(document.getElementById("root")).render(
