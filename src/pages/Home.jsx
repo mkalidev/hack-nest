3ab31beb3514f0146edd2ab36b2187012d4e1dd1
mkalidev <greatpannft@gmail.com>
2025-11-17 22:45:31 +0000

    feat: changes in src/pages/Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,7 +4,7 @@ import Container from "../components/layout/Container";
 export default function Home() {
   return (
     <>
-      <div className="w-ful h-full bg-purple-500">
+      <div className="w-full h-[30px] bg-purple-500">
         {/* <Container>
           <div className="w-full h-full bg-red-500">
             <h1>Home</h1>
