b68b38bf05f128f49f355a30fbed5f5858e67393
mkalidev <greatpannft@gmail.com>
2025-11-17 22:54:53 +0000

    feat: changes in src/components/libs/utils.js

--- src/components/libs/utils.js
+++ src/components/libs/utils.js
@@ -95,3 +95,10 @@ export const formatNumberCompact = (number, decimals = 1) => {
   return num.toString();
 };
 
+// Helper function to get cookie value
+export function getCookie(name) {
+  const value = `; ${document.cookie}`;
+  const parts = value.split(`; ${name}=`);
+  if (parts.length === 2) return parts.pop().split(";").shift();
+  return null;
+}
