0b6b56f4a322c30421bf46fd987a35b60c56c46e
mkalidev <greatpannft@gmail.com>
2025-11-17 21:44:48 +0000

    feat: changes in src/components/layout/Footer.jsx

---
+++ src/components/layout/Footer.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default function Footer() {
+  return (
+    <div className='fixed bottom-0 left-0 right-0 bg-white shadow-lg'>
+        <div className='flex justify-between items-center'>
+            <div className='flex items-center'>
+                <span className='text-xl font-bold'>Footer</span>
+            </div>
+        </div>
+    </div>
+  )
+}
