fa17cc7ef07da857120dd8c9fdb592dfda39195c
mkalidev <greatpannft@gmail.com>
2025-11-17 21:49:48 +0000

    feat: changes in src/components/layout/Sidebar.jsx

--- src/components/layout/Sidebar.jsx
+++ src/components/layout/Sidebar.jsx
@@ -1,15 +1,15 @@
-import React from 'react'
-import { RiFileChart2Fill } from 'react-icons/ri'
+import React from "react";
+import { RiFileChart2Fill } from "react-icons/ri";
 
 export default function Sidebar() {
   return (
-    <div className='fixed top-0 left-0 bottom-0 w-64 bg-white shadow-lg'>
-        <div className='flex flex-col items-center'>
-            <div className='flex items-center'>
-                <RiFileChart2Fill className='text-2xl' />
-                <span className='text-xl font-bold'>Dashboard</span>
-            </div>
+    <div className="fixed top-0 left-0 bottom-0 w-64 bg-white shadow-lg">
+      <div className="flex flex-col items-center">
+        <div className="flex items-center">
+          <RiFileChart2Fill className="text-2xl" />
+          <span className="text-xl font-bold">Hack Nest</span>
         </div>
+      </div>
     </div>
-  )
+  );
 }
