c6db07a91468c39511d426a0e1c4c4054d4c5f4d
mkalidev <greatpannft@gmail.com>
2025-11-17 23:32:53 +0000

    feat: changes in src/main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,26 +1,23 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
 // Supports weights 100-900
-import '@fontsource-variable/inter';
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
-import { AppKitProvider } from '@reown/appkit/react'
-import { baseSepolia } from 'viem/chains'
-import { base, celo, mainnet } from 'viem/chains'
+import "@fontsource-variable/inter";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { AppKitProvider } from "@reown/appkit/react";
+import { baseSepolia, base, celo, mainnet } from "viem/chains";
+const queryClient = new QueryClient();
 
-const queryClient = new QueryClient()
-
-createRoot(document.getElementById('root')).render(
-
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <AppKitProvider  projectId="39620c2d95c222279e45e3862fba2338"
-      networks={[
-       baseSepolia, base, celo, mainnet
-      ]}>
+    <AppKitProvider
+      projectId="39620c2d95c222279e45e3862fba2338"
+      networks={[baseSepolia, base, celo, mainnet]}
+    >
       <QueryClientProvider client={queryClient}>
         <App />
       </QueryClientProvider>
     </AppKitProvider>
-  </StrictMode>,
-)
+  </StrictMode>
+);
