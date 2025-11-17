cb947b8bc131d1800079a549f81af396bdf589e7
mkalidev <greatpannft@gmail.com>
2025-11-17 22:44:51 +0000

    feat: changes in src/pages/Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,14 +3,14 @@ import Container from "../components/layout/Container";
 
 export default function Home() {
   return (
-    <div className="w-full h-full">
-      <div className="w-full h-full bg-purple-500">
+    <>
+      <div className="w-ful h-full bg-purple-500">
         <Container>
           <div className="w-full h-full bg-red-500">
             <h1>Home</h1>
           </div>
         </Container>
       </div>
-    </div>
+    </>
   );
 }
