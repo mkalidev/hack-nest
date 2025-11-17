4d504fc4a13a9addbc6b02dfbe87fb1d1ffe1dd1
mkalidev <greatpannft@gmail.com>
2025-11-17 21:49:08 +0000

    feat: changes in src/App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,18 @@
-
-import './App.css'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import Layout from './components/layout/Layout'
-import Home from './pages/Home'
+import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Layout from "./components/layout/Layout";
+import Home from "./pages/Home";
 
 function App() {
-
   return (
     <>
-     <BrowserRouter>
-     <Routes element={<Layout />}>
-        <Route path="/" element={<Home />} />
-      </Routes>
-     </BrowserRouter>
+      <BrowserRouter>
+        <Routes element={<Layout />}>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </BrowserRouter>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
