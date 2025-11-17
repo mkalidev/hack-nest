dd26451c81e0a121d4b6437c46eb7008119a11ac
mkalidev <greatpannft@gmail.com>
2025-11-17 22:11:52 +0000

    feat: changes in src/components/layout/Navbar.jsx

--- src/components/layout/Navbar.jsx
+++ src/components/layout/Navbar.jsx
@@ -35,7 +35,7 @@ export default function Navbar() {
   return (
     <>
       <div className="w-full mx-auto py-5 bg-white sticky top-0 z-10 border-b border-black/10 h-fit md:h-[85.12px]">
-        <div className="mx-auto px-4 md:px-6 flex items-center h-full justify-between max-w-[1280px]">
+        <div className="mx-auto px-4 md:px-6 flex items-center h-full justify-between max-w-[1300px]">
           <Link to="/">
             <p className="font-semibold text-[16px] md:text-[20px]">
               Welcome,{" "}
