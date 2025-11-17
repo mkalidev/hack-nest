4cdfdc9290bb4e30bc40eec0c40805cc83c485aa
mkalidev <greatpannft@gmail.com>
2025-11-17 22:42:51 +0000

    feat: changes in src/pages/Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,8 +3,8 @@ import React from 'react'
 export default function Home() {
   return (
     <div className='w-full h-full'>
-      <div className='w-full h-full bg-red-500'>
-        
+      <div className='w-full h-full bg-purple-500'>
+
       </div>
     </div>
   )
