a6ff7bec05e43ec970caf5c36bcd572fc55f8fa4
mkalidev <greatpannft@gmail.com>
2025-11-17 21:49:48 +0000

    feat: changes in src/components/layout/MobileSidebar.jsx

--- src/components/layout/MobileSidebar.jsx
+++ src/components/layout/MobileSidebar.jsx
@@ -1,15 +1,21 @@
-import React from 'react'
-import { RiFileChart2Fill } from 'react-icons/ri'
+import React from "react";
+import { RiFileChart2Fill } from "react-icons/ri";
 
 export default function MobileSidebar() {
   return (
-    <div className='fixed bottom-0 left-0 right-0 bg-white shadow-lg'>
-        <div className='flex justify-between items-center'>
-            <div className='flex items-center'>
-                <RiFileChart2Fill className='text-2xl' />
-                <span className='text-xl font-bold'>Dashboard</span>
-            </div>
-        </div>
+    <div className="fixed bottom-0 left-0 right-0 bg-white shadow-lg">
+      <div className="flex flex-col items-center">
+        <div className="flex items-center">
+          <RiFileChart2Fill className="text-2xl" />
+          <span className="text-xl font-bold">Hack Nest</span>
+        </div>  
+      </div>
+      <div className="flex flex-col items-center">
+        <Link to="/">
+          <RiHome2Fill className="text-2xl" />
+          <span className="text-xl font-bold">Home</span>
+        </Link>
+      </div>
     </div>
-  )
+  );
 }
