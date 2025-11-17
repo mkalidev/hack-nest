5342c0f0e0981d8511ac173aeb62afd6a0282dec
mkalidev <greatpannft@gmail.com>
2025-11-17 22:43:11 +0000

    feat: changes in src/pages/Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,7 +4,9 @@ export default function Home() {
   return (
     <div className='w-full h-full'>
       <div className='w-full h-full bg-purple-500'>
-
+    <Container>
+      
+    </Container>
       </div>
     </div>
   )
