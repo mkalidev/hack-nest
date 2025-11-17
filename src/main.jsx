fff48d01b52d16915785db22e08e74e11ea55bb9
mkalidev <greatpannft@gmail.com>
2025-11-17 23:39:35 +0000

    feat: changes in src/main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -7,6 +7,8 @@ import "@fontsource-variable/inter";
 // import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { AppKitProvider } from "./Provider.jsx";
 
+// const queryClient = new QueryClient();
+
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <AppKitProvider>
