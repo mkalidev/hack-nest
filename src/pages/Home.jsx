3e2deb0f8ffede46abda4fc110d116fdcd2c475e
mkalidev <greatpannft@gmail.com>
2025-11-17 22:43:31 +0000

    feat: changes in src/pages/Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,13 +1,15 @@
 import React from 'react'
+import Container from '../components/layout/Container'
 
 export default function Home() {
   return (
     <div className='w-full h-full'>
       <div className='w-full h-full bg-purple-500'>
     <Container>
-      
-    </Container>
-      </div>
+        <div className='w-full h-full bg-red-500'>
+          <h1>Home</h1>
+        </div>
+      </Container>
     </div>
   )
 }
