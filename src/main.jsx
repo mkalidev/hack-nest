7f64baeba74eec8620ebffe7bd87a289edf32318
mkalidev <greatpannft@gmail.com>
2025-11-17 23:38:54 +0000

    feat: changes in src/main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -5,16 +5,12 @@ import App from "./App.jsx";
 // Supports weights 100-900
 import "@fontsource-variable/inter";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { AppKitProvider } from "@reown/appkit/react";
-import { baseSepolia, base, celo, mainnet } from "viem/
+import { AppKitProvider } from "./Provider.jsx";
 const queryClient = new QueryClient();
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <AppKitProvider
-      projectId="39620c2d95c222279e45e3862fba2338"
-      networks={[baseSepolia, base, celo, mainnet]}
-    >
+    <AppKitProvider>
       <QueryClientProvider client={queryClient}>
         <App />
       </QueryClientProvider>
