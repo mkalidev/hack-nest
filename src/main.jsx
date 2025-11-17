dbb8dcabb2b3816822ed0880e31bdbd9a3e88bac
mkalidev <greatpannft@gmail.com>
2025-11-17 23:31:16 +0000

    feat: changes in src/main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -5,16 +5,17 @@ import App from './App.jsx'
 // Supports weights 100-900
 import '@fontsource-variable/inter';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { AppKitProvider } from '@reown/appkit/react'
 
 const queryClient = new QueryClient()
 
 createRoot(document.getElementById('root')).render(
 
   <StrictMode>
-    <AppKitProvider>
-      
-    <QueryClientProvider client={queryClient}>
-      <App />
-    </QueryClientProvider>
+    <AppKitProvider >
+      <QueryClientProvider client={queryClient}>
+        <App />
+      </QueryClientProvider>
+    </AppKitProvider>
   </StrictMode>,
 )
