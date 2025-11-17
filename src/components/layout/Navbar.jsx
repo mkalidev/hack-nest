98a4887434f8c6e8f777691767d006764e59b44e
mkalidev <greatpannft@gmail.com>
2025-11-17 21:45:08 +0000

    feat: changes in src/components/layout/Navbar.jsx

---
+++ src/components/layout/Navbar.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default function Navbar() {
+  return (  
+    <nav className='fixed top-0 left-0 right-0 bg-white shadow-lg'>
+        <div className='flex justify-between items-center'>
+            <div className='flex items-center'>
+                <h1 className='text-2xl font-bold'>Hack Nest</h1>
+            </div>
+        </div>
+    </nav>
+  )
+}
