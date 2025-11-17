eb0df643109987c5bf573c4615695191512d7dfb
mkalidev <greatpannft@gmail.com>
2025-11-17 22:20:50 +0000

    feat: changes in src/components/layout/Navbar.jsx

--- src/components/layout/Navbar.jsx
+++ src/components/layout/Navbar.jsx
@@ -46,7 +46,7 @@ export default function Navbar() {
           </div>
           <div className="flex gap-3 items-center">
           <Link to="/login">
-          <p className="text-xs font-semibold">Bounty</p>
+          <p className="text-sm font-medium text-c-color">Bounty</p>
           </Link>
 
           </div>
