5d4ddd5ca1103be8c4cb30712a88c82c9e04dd83
mkalidev <greatpannft@gmail.com>
2025-11-17 22:30:51 +0000

    feat: changes in src/components/layout/Layout.jsx

--- src/components/layout/Layout.jsx
+++ src/components/layout/Layout.jsx
@@ -9,7 +9,7 @@ export default function Layout() {
       {/* <Sidebar /> */}
       <div className="space-y-0 w-full relative">
         <Navbar />
-        <div className="max-w-[1320px] py-10 px-4 md:px-6 mx-auto">
+        <div className="max-w-[1320px] py-5 px-4 md:px-6 mx-auto">
           <Outlet />
         </div>
       </div>
