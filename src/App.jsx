3a42bf7e3513d44b1a2d27a7509b61ad0a55a348
mkalidev <greatpannft@gmail.com>
2025-11-17 21:48:08 +0000

    feat: changes in src/App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 
 import './App.css'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Layout from './components/layout/Layout'
+import Home from './pages/Home'
 
 function App() {
 
