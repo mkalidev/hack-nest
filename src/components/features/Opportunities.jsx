b22ae0ff80ac61e43c5c115f87130864e6bd92ca
mkalidev <greatpannft@gmail.com>
2025-11-18 12:29:36 +0000

    feat: changes in src/components/features/Opportunities.jsx

--- src/components/features/Opportunities.jsx
+++ src/components/features/Opportunities.jsx
@@ -46,7 +46,7 @@ export default function Opportunities() {
               activeTab === tab.value
                 ? "bg-c-color text-white"
                 : "text-black/60 border border-black/10"
-            } px-4 py-2 rounded-xl cursor-pointer text-sm transition-all duration-300`}
+            } px-4 py-2 rounded-xl cursor-pointer text-sm hover:bg-c-color hover:text-black transition-all duration-300`}
             onClick={() => setActiveTab(tab.value)}
           >
             {tab.name}
