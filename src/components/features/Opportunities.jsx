bb3cb3e689275e2db51cad2a00355478bc66f29c
mkalidev <greatpannft@gmail.com>
2025-11-18 12:15:56 +0000

    feat: changes in src/components/features/Opportunities.jsx

--- src/components/features/Opportunities.jsx
+++ src/components/features/Opportunities.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 export default function Opportunities() {
   return (
-    <div className='w-full h-full bg-white rounded-lg p-4'>
+    <div className='w-full space-y-4'>
         <h2 className='text-2xl font-bold'>Latest Opportunities</h2>
         <p className='text-sm text-gray-500'>
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quos.
