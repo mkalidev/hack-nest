2fb1fba143c95757899d49d46869fc35b6bb2133
mkalidev <greatpannft@gmail.com>
2025-11-18 12:16:16 +0000

    feat: changes in src/components/features/Opportunities.jsx

--- src/components/features/Opportunities.jsx
+++ src/components/features/Opportunities.jsx
@@ -3,6 +3,9 @@ import React from 'react'
 export default function Opportunities() {
   return (
     <div className='w-full space-y-4'>
+        <div className="w-full flex gap-5 items-center justify-between">
+            
+        </div>
         <h2 className='text-2xl font-bold'>Latest Opportunities</h2>
         <p className='text-sm text-gray-500'>
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quos.
