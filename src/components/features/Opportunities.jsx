d1bcb2ec6fc30248a552ba1954d0d8521d8aad70
mkalidev <greatpannft@gmail.com>
2025-11-18 12:16:36 +0000

    feat: changes in src/components/features/Opportunities.jsx

--- src/components/features/Opportunities.jsx
+++ src/components/features/Opportunities.jsx
@@ -4,9 +4,11 @@ export default function Opportunities() {
   return (
     <div className='w-full space-y-4'>
         <div className="w-full flex gap-5 items-center justify-between">
-            
-        </div>
         <h2 className='text-2xl font-bold'>Latest Opportunities</h2>
+<div className="flex gap-3 items-center">
+    <p>Filter</p>
+</div>
+        </div>
         <p className='text-sm text-gray-500'>
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quos.
         </p>
