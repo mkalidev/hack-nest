1e21b8e928dbc84eb9f2d9bccd6d8fac4db69e99
mkalidev <greatpannft@gmail.com>
2025-11-18 12:16:56 +0000

    feat: changes in src/components/features/Opportunities.jsx

--- src/components/features/Opportunities.jsx
+++ src/components/features/Opportunities.jsx
@@ -1,17 +1,19 @@
-import React from 'react'
+import React from "react";
+import { RiFilter3Fill } from "react-icons/ri";
 
 export default function Opportunities() {
   return (
-    <div className='w-full space-y-4'>
-        <div className="w-full flex gap-5 items-center justify-between">
-        <h2 className='text-2xl font-bold'>Latest Opportunities</h2>
-<div className="flex gap-3 items-center">
-    <p>Filter</p>
-</div>
+    <div className="w-full space-y-4">
+      <div className="w-full flex gap-5 items-center justify-between">
+        <h2 className="text-2xl font-bold">Latest Opportunities</h2>
+        <div className="flex gap-3 items-center">
+          <RiFilter3Fill />
+          <p>Filter</p>
         </div>
-        <p className='text-sm text-gray-500'>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quos.
-        </p>
+      </div>
+      <p className="text-sm text-gray-500">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quos.
+      </p>
     </div>
-  )
+  );
 }
