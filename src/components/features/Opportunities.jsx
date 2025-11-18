0250accb6e363c5ddd7b69eed12560cbb02f5b8a
mkalidev <greatpannft@gmail.com>
2025-11-18 14:15:39 +0000

    feat: changes in src/components/features/Opportunities.jsx

--- src/components/features/Opportunities.jsx
+++ src/components/features/Opportunities.jsx
@@ -66,7 +66,10 @@ export default function Opportunities() {
                 alt={""}
                 className="w-24 h-24 bg-c-color/10 object-cover"
               />
-              <h3 className="text-lg font-semibold">{item.company}</h3>
+              <div className="flex flex-col gap-2">
+                <h3 className="text-lg font-semibold">{item.company}</h3>
+                <p className="text-sm text-black/60">{item.companyLocation}</p>
+              </div>
             <h3 className="text-lg font-semibold">{item.title}</h3>
             </div>
             <div className="flex gap-2 items-center">
