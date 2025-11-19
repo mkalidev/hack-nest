c75cf6cb911ac1098b32413b4692457da5877525
mkalidev <greatpannft@gmail.com>
2025-11-19 21:44:14 +0000

    feat: changes in src/pages/Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,6 +4,7 @@ import Opportunities from "../components/features/Opportunities";
 export default function Home() {
   return (
     <>
+    <div className="w-full flex flex-col gap-4"></div>
       <Opportunities />
     </>
   );
