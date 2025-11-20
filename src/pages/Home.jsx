cb34a928b5951d1f06fb4b3e309729e73f80cda1
mkalidev <greatpannft@gmail.com>
2025-11-20 19:58:56 +0000

    feat: changes in src/pages/Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,13 +1,16 @@
 import React from "react";
 import Opportunities from "../components/features/Opportunities";
+import StatsSidebar from "../components/features/StatsSidebar";
 
 export default function Home() {
   return (
     <>
-    <div className="w-full flex flex-col md:flex-row items-top gap-4 relative h-auto">
-      <Opportunities />
-      
-    </div>
+      <div className="w-full flex flex-col md:flex-row items-start gap-6 relative h-auto">
+        <div className="flex-1">
+          <Opportunities />
+        </div>
+        <StatsSidebar />
+      </div>
     </>
   );
 }
